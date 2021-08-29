afa011dcc7095283192c57c0f46a076f8cdb754a
PaoloTorregroza <paolinskypdtn@outlook.com>
2021-08-29 13:55:57 -0500

    Update settings

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ const getOptions = () => {
 const ormConfig = getOptions()
 createConnection(ormConfig)
   .then(() => {
-    app.listen(process.env.PORT || port, () => console.log(`App running on port ${port}. . .`))
+    app.listen(process.env.PORT || port, () => console.log(`App running on port ${process.env.PORT || port}. . .`))
   })
   .catch((err) => {
     console.log('Unable to connect to db', err)
